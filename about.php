7c728befbe49b8de602abb0c746ae8cd0c02f5f3
Ivan Sefcik <isefcik@centrum.sk>
2019-10-12 13:41:17 +0200

    update of the about me article

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang='sk'>
+  <head>
+    <title>Ivan Šefčík - O mne</title>    
+      <link rel="stylesheet" type="text/css" href="./css/style_CSS.css">
+      <link rel="icon" href="./images/favicon2.jpg">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">     <!-- icon css library -->
+      <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Raleway:300" rel="stylesheet">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset='utf-8'>    
+      <meta name='author' content='Ivan Sefcik'>
+      <meta name='robots' content='all'>
+      <script>                    
+          var pageNameString = 'about';            
+      </script>
+      <script type="text/javascript" src="./script/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="./script/jsFunctions.js"></script>
+      
+  </head>    
+    <body>  
+        <div id="header-phantom"></div>
+        <header class="header"><?php include "php/header.php"; ?></header> 
+        <div class="content">       
+            <div class="content-text-fade">
+                <h1>O mne</h1>                
+            </div>            
+            <div class="aboutme">             
+                <div class="photo-container">
+                    <div class="my-photo"></div>
+                </div>
+                <hr>
+                <article>
+                    <div class="text-about">
+                        <div class="margin center-text">
+                            <p>
+                                <strong>absolvent Slovenskej Technickej Univerzity v Bratislave</strong>
+                            </p>
+                            <p>
+                                <strong>Fakulta elektrotechniky a informatiky</strong>
+                            </p>
+                            <p>Dosiahnuté vzdelanie: <br> <span>Inžiniersky titul z Aplikovanej Informatiky</span></p>
+                            <hr>                            
+                        </div>
+                        <div style="text-align: center;"> 
+                        <p>
+                            Ahoj, volám sa Ivan a teším sa z tvojej prítomnosti v mojom malom portfóliu.<br> 
+                            Nájdeš tu ukážku mojej fotografickej tvorby a ďaľšie odkazy na moje sociálne účty.<br><br>
+                            <span style="font-weight: 700;">Počas prechádzok a výletov som zistil ako fotiť prírodu. Počas trávenia voľného času s kamarátmi som zistil ako
+                            fotiť portréty a počas spoznávania nových ľudí som zistil ako fotiť svatby a narodeninové oslavy.</span><br><br>
+                            Ak sa ti môj štýl fotenia páči a chcel by si odo mňa fotky<a href="./laboratory.php">,</a> neváhaj mi napísať a môžme spolu
+                            splniť tvoje predstavy. 
+                        </p>
+                            
+                        </div>
+                        
+                        <br><br>
+                        
+                        <div class="center-text"><hr></div>
+                        <div class="contact-link" >
+                            <h3>Poďme si písať, napíš mi!</h3>
+                            <a href="./contact.php"><i class="fa fa-envelope"></i></a>
+                        </div>
+                    </div>
+                    
+                </article>
+            </div>
+                
+        </div>                
+        <footer><?php include "php/footer.php"; ?></footer> 
+    </body>
+</html>
